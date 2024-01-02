f4889aefca78f4fb9f92d8ab20621fb2c28b2575
Emilia Rojo <emiliamrojo@gmail.com>
2024-01-02 18:02:55 +0100

    Responsive fixes and style changes

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -5,12 +5,14 @@ export default class extends Controller {
   connect() {
      this.menuTarget.style.transition = "transform 0.5s ease-in-out";
   }
-  static targets = ["menu"];
+  static targets = ["menu", "body"];
 
   showMenu() {
     this.menuTarget.style.transform = "translateX(0%)";
+    this.bodyTarget.style.overflowY = "hidden";
   }
   hideMenu() {
     this.menuTarget.style.transform = "translateX(-100%)";
+    this.bodyTarget.style.overflowY = "auto";
   }
 }
